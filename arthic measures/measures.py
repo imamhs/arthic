34e22445ff9b3d5a82e6016b221a9ef79c8ad649
Imam <emamhd@gmail.com>
2024-04-05 19:08:34 +1100

    Add files via upload

---
+++ arthic measures/measures.py
@@ -0,0 +1,18 @@
+# Arthic measures library
+# Copyright (c) 2024, Md Imam Hossain (emamhd at gmail dot com)
+# see LICENSE.txt for details
+
+"""
+ Lifelong wealth building financial formulas
+"""
+
+def A_calculate_growth_annually(_credit, _frequency, _period, _growth=0.0):
+
+    Final_generated_balance = 0
+
+    for yp in range(_period):
+        Final_generated_balance += (_credit*_frequency)
+        Final_generated_balance += (Final_generated_balance*_growth)
+
+    return round(Final_generated_balance, 3)
+
